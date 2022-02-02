de850a1a435437243d3ac469adc38a098dc06756
paluszekNN <adampaluch95@gmail.com>
2022-02-02 21:01:16 +0100

    add linear regression app

--- mlAnalyse/urls.py
+++ mlAnalyse/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('linear_regression/', include('linear_regression.urls')),
     path('admin/', admin.site.urls),
 ]
