29274ebee2c3fe5444e720f4d6e00e2bbb4f40b3
paluszekNN <adampaluch95@gmail.com>
2022-02-05 11:15:08 +0100

    add index.html with update form

--- mlAnalyse/urls.py
+++ mlAnalyse/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import home_view
 
 urlpatterns = [
     path('linear_regression/', include('linear_regression.urls')),
     path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+
 ]
