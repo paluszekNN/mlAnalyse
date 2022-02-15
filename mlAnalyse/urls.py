db39c4f5e34cf665b874ec0f8675c38f3297fb22
paluszekNN <adampaluch95@gmail.com>
2022-02-15 19:57:52 +0100

    create view to update csv file

--- mlAnalyse/urls.py
+++ mlAnalyse/urls.py
@@ -15,11 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home_view
+from .views import data_upload
 
 urlpatterns = [
     path('linear_regression/', include('linear_regression.urls')),
     path('admin/', admin.site.urls),
-    path('', home_view, name='home'),
-
+    path('', data_upload, name='home'),
 ]
--- mlAnalyse/views.py
+++ mlAnalyse/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
+from django.contrib import messages
+import csv, io
+import pandas as pd
 
 
-def home_view(request, *args, **kwargs):
-    return render(request, 'index.html', {})
+def data_upload(request):
+    template = 'index.html'
+
+    prompt = {
+        'order': ''
+    }
+
+    if request.method == "GET":
+        return render(request, template, prompt)
+
+    csv_file = request.FILES['file']
+
+    if not csv_file.name.endswith('.csv'):
+        messages.error(request, 'This is not a csv file')
+
+    try:
+        data = pd.read_csv(csv_file)
+        print(data.head(2))
+    except:
+        messages.error(request, 'This file can\'t export as data')
+
+    context = {}
+    return render(request, template, context)
