6a1e57fa498ecc089b1f4b47b25c40476d9ac387
paluszekNN <adampaluch95@gmail.com>
2022-02-19 14:24:21 +0100

    Add linear models to show coef, bias and score of models

--- linear_regression/urls.py
+++ linear_regression/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('', views.DataView.as_view(), name='data'),
+    path('lr', views.LRView.as_view(), name='lr'),
 ]
--- linear_regression/views.py
+++ linear_regression/views.py
@@ -5,10 +5,14 @@ from django.http import HttpResponse
 from django.views import generic
 from .models import Data
 import pandas as pd
+from sklearn.linear_model import LinearRegression, ElasticNet, Ridge, SGDRegressor, Lars, Lasso
+from sklearn.model_selection import KFold
+from sklearn.metrics import r2_score
+import numpy as np
 
 
-class IndexView(generic.ListView):
-    template_name = 'ml.html'
+class DataView(generic.ListView):
+    template_name = 'data.html'
     context_object_name = 'data'
 
     def get_queryset(self):
@@ -26,4 +30,55 @@ class IndexView(generic.ListView):
         return context
 
 
+class LRView(generic.ListView):
+    template_name = 'lr.html'
+    context_object_name = 'data'
 
+    def get_queryset(self):
+        return Data.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        df = pd.DataFrame(json.loads(Data.objects.all()[0].data))
+        df.dropna(inplace=True)
+        for col in df.columns:
+            try:
+                df[col].astype(float)
+            except:
+                df.drop(col, axis=1, inplace=True)
+        lr = LinearRegression()
+        el1 = ElasticNet(1)
+        el05 = ElasticNet(0.5)
+        el025 = ElasticNet(0.25)
+        el01 = ElasticNet(0.1)
+        lasso = Lasso()
+        ridge = Ridge()
+        sgd = SGDRegressor()
+        lars = Lars()
+
+        models = [lr, el1, el05, el025, el01, lasso, ridge, sgd, lars]
+        data_X = df.drop(df.columns[-1], axis=1)
+        data_y = df[df.columns[-1]]
+        kf = KFold(n_splits=5, random_state=None, shuffle=False)
+        kf.get_n_splits(data_X)
+        models_mean_score = []
+        coefs = []
+        biases = []
+        model_names = []
+        for model in models:
+            scores = []
+            for train, test in kf.split(data_X):
+                model.fit(data_X.iloc[train], data_y.iloc[train])
+                scores.append(r2_score(model.predict(data_X.iloc[test]), data_y.iloc[test]))
+            models_mean_score.append(np.mean(scores))
+            coefs.append(np.round(model.coef_, decimals=4))
+            biases.append(float(model.intercept_))
+            model_names.append(str(model))
+        p = coefs[0].argsort()[::-1]
+        for i in range(coefs.__len__()):
+            coefs[i] = coefs[i][p]
+        data = zip(model_names, np.round(models_mean_score, decimals=4), np.round(biases, decimals=4), coefs)
+
+        context['data_0'] = data
+        context['features'] = np.array(df.columns[:-1])[p]
+        return context
