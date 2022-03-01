84ad227b812f22bac7f7b60c7696637e3f39f9c1
paluszekNN <adampaluch95@gmail.com>
2022-03-01 13:06:36 +0100

    create pattern strategy for different type of models

--- linear_regression/views.py
+++ linear_regression/views.py
@@ -32,22 +32,16 @@ class DataView(generic.ListView):
         return context
 
 
-class LRView(generic.ListView):
-    template_name = 'lr.html'
-    context_object_name = 'data'
+class TypeOfModeling:
+    def get_models(self):
+        pass
 
-    def get_queryset(self):
-        return Data.objects.all()
+    def get_attributes(self, model):
+        pass
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)
-        df = pd.DataFrame(json.loads(Data.objects.all()[0].data))
-        df.dropna(inplace=True, axis=1)
-        for col in df.columns:
-            try:
-                df[col] = df[col].astype(float)
-            except:
-                df.drop(col, axis=1, inplace=True)
+
+class LinearModels(TypeOfModeling):
+    def get_models(self):
         lr = LinearRegression
         el1 = ElasticNet
         el05 = ElasticNet
@@ -58,43 +52,108 @@ class LRView(generic.ListView):
         sgd = SGDRegressor
         lars = Lars
         list_of_elasticnet_alpha = [1, 0.5, 0.25, 0.1]
-
         models = [lr, el1, el05, el025, el01, lasso, ridge, sgd, lars]
-        data_X = df.drop(df.columns[-1], axis=1)
-        data_y = df[df.columns[-1]]
+        return list_of_elasticnet_alpha, models
+
+    def get_attributes(self, model):
+        return model.coef_, model.intercept_
+
+
+class TreeModels(TypeOfModeling):
+    def get_attributes(self, model):
+        return model.feature_importances_,
+
+    def get_models(self):
+        ex = ExtraTreeRegressor
+        dtr = DecisionTreeRegressor
+
+        models = [ex, dtr]
+        return None, models
+
+
+class MLModels:
+    def __init__(self, type_of_modeling):
+        self._type_of_modeling = type_of_modeling
+        df = pd.DataFrame(json.loads(Data.objects.all()[0].data))
+        df.dropna(inplace=True, axis=1)
+        for col in df.columns:
+            try:
+                df[col] = df[col].astype(float)
+            except:
+                df.drop(col, axis=1, inplace=True)
+        self.data = df
+
+    @property
+    def type_of_modeling(self) -> TypeOfModeling:
+        return self._type_of_modeling
+
+    @type_of_modeling.setter
+    def type_of_modeling(self, type_of_modeling: TypeOfModeling) -> None:
+        self._type_of_modeling = type_of_modeling
+
+    def get_attributes(self):
+        data_X = self.data.drop(self.data.columns[-1], axis=1)
+        data_y = self.data[self.data.columns[-1]]
         kf = KFold(n_splits=5, random_state=None, shuffle=True)
         kf.get_n_splits(data_X)
-        models_mean_score = []
-        coefs = []
-        biases = []
+
+        attributes = {}
         model_names = []
+        models_mean_score = []
+
+        list_of_elastic_net_alpha, models = self._type_of_modeling.get_models()
         for model in models:
             scores = []
+            el_alpha = None
             if model == ElasticNet:
-                alpha = list_of_elasticnet_alpha.pop()
+                el_alpha = list_of_elastic_net_alpha.pop()
             for train, test in kf.split(data_X):
                 if model == ElasticNet:
-                    model_to_fit = model(alpha)
+                    model_to_fit = model(el_alpha)
                 else:
                     model_to_fit = model()
                 model_to_fit.fit(data_X.iloc[train], data_y.iloc[train])
                 scores.append(r2_score(model_to_fit.predict(data_X.iloc[test]), data_y.iloc[test]))
-            models_mean_score.append(np.mean(scores))
+            models_mean_score.append(np.round(np.mean(scores), decimals=4))
             if model == ElasticNet:
-                model_to_fit = model(alpha)
+                model_to_fit = model(el_alpha)
             else:
                 model_to_fit = model()
             model_to_fit.fit(data_X, data_y)
-            biases.append(float(model_to_fit.intercept_))
-            coefs.append(np.round(model_to_fit.coef_, decimals=4))
+            i = 0
+            for att in self._type_of_modeling.get_attributes(model_to_fit):
+                if str(i) not in attributes.keys():
+                    attributes[str(i)] = []
+                attributes[str(i)].append(np.round(att, decimals=4))
+                i += 1
             model_names.append(str(model_to_fit))
-        p = coefs[np.argmax(models_mean_score)].argsort()[::-1]
-        for i in range(coefs.__len__()):
-            coefs[i] = coefs[i][p]
-        data = zip(model_names, np.round(models_mean_score, decimals=4), np.round(biases, decimals=4), coefs)
+        assert isinstance(attributes['0'][0], np.ndarray)
+        sorted_indexes_by_score = attributes['0'][np.argmax(models_mean_score)].argsort()[::-1]
 
-        context['data_0'] = data
-        context['features'] = np.array(df.columns[:-1])[p]
+        for i in range(attributes['0'].__len__()):
+            attributes['0'][i] = attributes['0'][i][sorted_indexes_by_score]
+
+        return (model_names, *attributes.values(), models_mean_score), sorted_indexes_by_score
+
+
+class LRView(generic.ListView):
+    template_name = 'lr.html'
+    context_object_name = 'data'
+
+    def get_queryset(self):
+        return Data.objects.all()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        ml = MLModels(LinearModels())
+        attributes, sorted_indexes_by_score = ml.get_attributes()
+
+        for att in attributes:
+            assert isinstance(att, list)
+
+        context['attributes'] = zip(*attributes)
+        context['features'] = np.array(ml.data.columns[:-1])[sorted_indexes_by_score]
         return context
 
 
@@ -107,40 +166,9 @@ class TSMView(generic.ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        df = pd.DataFrame(json.loads(Data.objects.all()[0].data))
-        df.dropna(inplace=True, axis=1)
-        for col in df.columns:
-            try:
-                df[col] = df[col].astype(float)
-            except:
-                df.drop(col, axis=1, inplace=True)
-        ex = ExtraTreeRegressor
-        dtr = DecisionTreeRegressor
 
-        models = [ex, dtr]
-        data_X = df.drop(df.columns[-1], axis=1)
-        data_y = df[df.columns[-1]]
-        kf = KFold(n_splits=5, random_state=None, shuffle=True)
-        kf.get_n_splits(data_X)
-        models_mean_score = []
-        coefs = []
-        model_names = []
-        for model in models:
-            scores = []
-            for train, test in kf.split(data_X):
-                model_to_fit = model()
-                model_to_fit.fit(data_X.iloc[train], data_y.iloc[train])
-                scores.append(r2_score(model_to_fit.predict(data_X.iloc[test]), data_y.iloc[test]))
-            models_mean_score.append(np.mean(scores))
-            model_to_fit = model()
-            model_to_fit.fit(data_X, data_y)
-            coefs.append(np.round(model_to_fit.feature_importances_, decimals=4))
-            model_names.append(str(model_to_fit))
-        p = coefs[np.argmax(models_mean_score)].argsort()[::-1]
-        for i in range(coefs.__len__()):
-            coefs[i] = coefs[i][p]
-        data = zip(model_names, np.round(models_mean_score, decimals=4), coefs)
-
-        context['data_0'] = data
-        context['features'] = np.array(df.columns[:-1])[p]
+        ml = MLModels(TreeModels())
+        attributes, sorted_indexes_by_score = ml.get_attributes()
+        context['attributes'] = zip(*attributes)
+        context['features'] = np.array(ml.data.columns[:-1])[sorted_indexes_by_score]
         return context
